7d371f8049afea39dff64ec282f715b3ff42e05b
“Ahmad <ahmadjunaidibeds@gmail.com>
2025-02-19 19:57:22 +0700

    Membuat Theme Bagian 1

---
+++ lib/shared/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Color whiteColor = Color(0xffFFFFFF);
+Color blackColor = Color(0xff14193F);
+Color greyColor = Color(0xffA4A8AE);
+Color LightBackgroundColor = Color(0xffF6F8FB);
+Color darkBackgroundColor = Color(0xff020518);
+Color blueColor = Color(0xff53C1F9);
+Color purpleColor = Color(0xff5142E6);
+Color greenColor = Color(0xff22B07D);
+Color numberBackgroundColor = Color(0xff1A1D2E);
