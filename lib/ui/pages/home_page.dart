a6f98a2bb24e3cc00efe461ecddec9c82baf6232
“Ahmad <ahmadjunaidibeds@gmail.com>
2025-03-17 14:49:30 +0700

    Friendly Tips 1

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:bank_sha/shared/theme.dart';
 import 'package:bank_sha/ui/widgets/home_latest_transaction_item.dart';
 import 'package:bank_sha/ui/widgets/home_service_item.dart';
+import 'package:bank_sha/ui/widgets/home_tips_item.dart';
 import 'package:bank_sha/ui/widgets/home_user_item.dart';
 import 'package:flutter/material.dart';
 
@@ -86,6 +87,7 @@ class HomePage extends StatelessWidget {
           buildServices(),
           buildLatestTranscitions(),
           buildSendAgain(),
+          buildFriendlyTips(),
         ],
       ),
     );
@@ -415,6 +417,33 @@ class HomePage extends StatelessWidget {
               ],
             ),
           )
+        ],
+      ),
+    );
+  }
+
+  Widget buildFriendlyTips() {
+    return Container(
+      margin: EdgeInsets.only(
+        top: 30,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Friendly Tips',
+            style: blackTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: semibold,
+            ),
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          HomeTipsItem(
+              imageUrl: 'assets/img_tips1.png',
+              title: 'Best tips for using a credit card',
+              url: 'https://www.google.com'),
         ],
       ),
     );
