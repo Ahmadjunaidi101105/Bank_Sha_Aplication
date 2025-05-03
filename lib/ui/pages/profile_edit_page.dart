718ffc152e81aeedb556dc8e9ec3b020803d240f
“Ahmad <ahmadjunaidibeds@gmail.com>
2025-05-03 22:16:42 +0700

    Slicing Edit Profile Page (Slicing Profile & Pin Page)

--- lib/ui/pages/profile_edit_page.dart
+++ lib/ui/pages/profile_edit_page.dart
@@ -1,3 +1,6 @@
+import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/ui/widgets/buttons.dart';
+import 'package:bank_sha/ui/widgets/forms.dart';
 import 'package:flutter/material.dart';
 
 class ProfileEditPage extends StatelessWidget {
@@ -5,8 +8,59 @@ class ProfileEditPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Edit Profile',
+        ),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+        ),
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          Container(
+            padding: const EdgeInsets.all(22),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: whiteColor,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const CustomFormFilled(title: 'Username'),
+                const SizedBox(
+                  height: 16,
+                ),
+                const CustomFormFilled(title: 'Full Name'),
+                const SizedBox(
+                  height: 16,
+                ),
+                const CustomFormFilled(title: 'Email Addres'),
+                const SizedBox(
+                  height: 16,
+                ),
+                const CustomFormFilled(
+                  title: 'Password',
+                  obscureText: true,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                CustomFilledButton(
+                  title: 'Continue',
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/sign-up-Set-profile');
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
