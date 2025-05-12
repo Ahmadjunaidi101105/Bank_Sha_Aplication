3746a27efb37d5e2977dd746c2d1af2414e18720
“Ahmad <ahmadjunaidibeds@gmail.com>
2025-05-12 15:58:59 +0700

    Melakukan Implementasi Sign Up Page (Consume API Authentication Flow)

--- lib/ui/pages/sign_up_page.dart
+++ lib/ui/pages/sign_up_page.dart
@@ -1,8 +1,12 @@
+import 'package:bank_sha/blocs/auth/auth_bloc.dart';
+import 'package:bank_sha/models/sign_up_form_model.dart';
 import 'package:bank_sha/shared/shared_methods.dart';
 import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/ui/pages/sign_up_Set_profile_page.dart';
 import 'package:bank_sha/ui/widgets/buttons.dart';
 import 'package:bank_sha/ui/widgets/forms.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -28,94 +32,126 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 24,
-        ),
-        children: [
-          Container(
-            height: 50,
-            width: 155,
-            margin: const EdgeInsets.only(
-              bottom: 100,
-              top: 100,
-            ),
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/img_logo_light.png'),
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is AuthFailed) {
+            showCustomSnackBar(context, state.e);
+          }
+          if (state is AuthCheckEmailSucces) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SignUpSetProfilePage(
+                  data: SignUpFormModel(
+                    name: nameController.text,
+                    email: emailController.text,
+                    password: passwordController.text,
+                  ),
+                ),
               ),
+            );
+          }
+        },
+        builder: (context, state) {
+          if (state is AuthLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return ListView(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 24,
             ),
-          ),
-          Text(
-            'Join Us to Unlock\nYour Growth',
-            style: blackTextStyle.copyWith(
-              fontWeight: semibold,
-              fontSize: 20,
-            ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          Container(
-            padding: const EdgeInsets.all(22),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: whiteColor,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // Note Name INput
-                CustomFormFilled(
-                  title: 'Full Name',
-                  controller: nameController,
+            children: [
+              Container(
+                height: 50,
+                width: 155,
+                margin: const EdgeInsets.only(
+                  bottom: 100,
+                  top: 100,
                 ),
-                const SizedBox(
-                  height: 16,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/img_logo_light.png'),
+                  ),
                 ),
-                // Note EMail : INput
-                CustomFormFilled(
-                  title: 'Email Addres',
-                  controller: nameController,
+              ),
+              Text(
+                'Join Us to Unlock\nYour Growth',
+                style: blackTextStyle.copyWith(
+                  fontWeight: semibold,
+                  fontSize: 20,
                 ),
-                const SizedBox(
-                  height: 16,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Container(
+                padding: const EdgeInsets.all(22),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: whiteColor,
                 ),
-                // Note PASSWORD : INput
-                CustomFormFilled(
-                  title: 'Password',
-                  obscureText: true,
-                  controller: passwordController,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Note Name INput
+                    CustomFormFilled(
+                      title: 'Full Name',
+                      controller: nameController,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    // Note EMail : INput
+                    CustomFormFilled(
+                      title: 'Email Addres',
+                      controller: emailController,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    // Note PASSWORD : INput
+                    CustomFormFilled(
+                      title: 'Password',
+                      obscureText: true,
+                      controller: passwordController,
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    CustomFilledButton(
+                      title: 'Continue',
+                      onPressed: () {
+                        if (validate()) {
+                          context
+                              .read<AuthBloc>()
+                              .add(AuthCheckEmail(emailController.text));
+                        } else {
+                          showCustomSnackBar(
+                              context, 'semua field harus diisi');
+                        }
+                      },
+                    ),
+                  ],
                 ),
-                const SizedBox(
-                  height: 30,
-                ),
-                CustomFilledButton(
-                  title: 'Continue',
-                  onPressed: () {
-                    if (validate())
-                      Navigator.pushNamed(context, '/sign-up-Set-profile');
-                    else {
-                      showCustomSnackBar(context, 'semua field harus diisi');
-                    }
-                  },
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          CustomTextButton(
-            title: 'Sign In',
-            onPressed: () {
-              Navigator.popAndPushNamed(context, '/sign-in');
-            },
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-        ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              CustomTextButton(
+                title: 'Sign In',
+                onPressed: () {
+                  Navigator.popAndPushNamed(context, '/sign-in');
+                },
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+            ],
+          );
+        },
       ),
     );
   }
